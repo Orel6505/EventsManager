d06ea3a1d846efa40997072db518383b2e838e57
Orel Yosupov <Orely4242@gmail.com>
2023-10-27 12:08:42 +0300

    EventsManager: EventsManagerModels: add Food.cs

---
+++ EventsManagerModels/Food.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsManagerModels
+{
+    public class Food
+    {
+        public int FoodId { get; set; }
+        public string FoodName { get; set; }
+        public string FoodDesc { get; set; }
+        public int FoodPrice { get; set; }
+        public string FoodImage { get; set; }
+    }
+}
