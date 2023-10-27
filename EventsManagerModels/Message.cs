4b931e9a6c1bf3a9f5c46c7646b79ac28bf21f04
Orel Yosupov <Orely4242@gmail.com>
2023-10-27 12:18:25 +0300

    EventsManager: EventsManagerModels: add Message.cs

---
+++ EventsManagerModels/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsManagerModels
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public string MessageContent { get; set; }
+        public string MessageDate { get; set; }
+    }
+}
