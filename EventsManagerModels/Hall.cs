2656e186f117fa19ba6f3b3caf6de169032e64e2
Orel Yosupov <Orely4242@gmail.com>
2023-10-27 12:15:11 +0300

    EventsManager: EventsManagerModels: add Hall.cs

---
+++ EventsManagerModels/Hall.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsManagerModels
+{
+    public class Hall
+    {
+        public int HallId { get; set; }
+        public string HallName { get; set; }
+        public string HallDesc { get; set; }
+        public int MaxPeople { get; set; }
+        public string HallImage { get; set; }
+    }
+}
