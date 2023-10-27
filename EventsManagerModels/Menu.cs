8981ce0179a2a76d772c24420229b4266330bf6b
Orel Yosupov <Orely4242@gmail.com>
2023-10-27 12:16:47 +0300

    EventsManager: EventsManagerModels: add Menu.cs

---
+++ EventsManagerModels/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsManagerModels
+{
+    public class Menu
+    {
+        public int MenuId { get; set; }
+        public string MenuName { get; set; }
+        public string HallDesc { get; set; }
+        public int MenuPrice { get; set; }
+        public string MenuImage { get; set; }
+    }
+}
