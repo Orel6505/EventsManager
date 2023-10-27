418c843e11911f9f006f363fdef8cc071ec39c44
Orel Yosupov <Orely4242@gmail.com>
2023-10-27 13:33:34 +0300

    EventsManager: EventsManagerModels: add Order.cs

---
+++ EventsManagerModels/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsManagerModels
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string OrderDate { get; set; }
+        public int MenuId { get; set; }
+        public int HallId { get; set; }
+        public int UserId { get; set; }
+        public int RatingId { get; set; }
+        public int NumOfPeople { get; set; }
+    }
+}
