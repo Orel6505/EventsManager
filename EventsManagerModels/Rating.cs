79dcd663496213a1d551c88892cef6f37062bfff
Orel Yosupov <Orely4242@gmail.com>
2023-10-27 13:45:46 +0300

    EventsManager: EventsManagerModels: add Rating.cs

---
+++ EventsManagerModels/Rating.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsManagerModels
+{
+    public class Rating
+    {
+        public int RatingId { get; set; }
+        public string RatingTitle { get; set; }
+        public string RatingDesc { get; set; }
+        public int RatingStars { get; set; }
+        public int UserId { get; set; }
+        public int RatingImages { get; set; }
+    }
+}
