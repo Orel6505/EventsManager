1c37574d878c8ae3ba442915b43ad6b5498ed7c0
Orel Yosupov <Orely4242@gmail.com>
2023-11-03 11:56:50 +0200

    EventsManager: EventsManagerServer: add IDBcontext.cs

---
+++ EventsManagerServer/Data_Access_Layer/IDBcontext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsManager.Data_Access_Layer
+{
+    public interface IDBcontext
+    {
+        void OpenConnection();
+        void CloseConnection();
+
+        void CreateCommand();
+        void DeleteCommand();
+
+        //CRUD = Create Read Update Delete
+        IDataReader Read(string sql);
+
+        bool Create(string sql);
+
+        bool update(string sql);
+
+        bool delete(string sql);
+
+    }
+}
