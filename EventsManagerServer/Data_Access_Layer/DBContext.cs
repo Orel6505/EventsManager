2f9a7dbbe65d2b23e0f4b5d32f8cf610a12dc7a7
Orel Yosupov <Orely4242@gmail.com>
2023-11-10 11:42:23 +0200

    EventsManager: EventsManagerServer: Update DBContext.cs
    create empty methods required from the inherithed interface

--- EventsManagerServer/Data_Access_Layer/DBContext.cs
+++ EventsManagerServer/Data_Access_Layer/DBContext.cs
@@ -1,11 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 
 namespace EventsManager.Data_Access_Layer
 {
-    public class DBContext
+    public abstract class DBContext : IDBcontext
     {
+        protected IDbConnection connection;
+        public void BeginTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CloseConnection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Create(string sql)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateCommand()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(string sql)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCommand()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EndTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OpenConnection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataReader Read(string sql)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ReadValue(string sql)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(string sql)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
