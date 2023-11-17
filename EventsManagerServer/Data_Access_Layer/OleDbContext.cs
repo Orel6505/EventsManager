5769d9b8b2447dc5fe1a44ac9a85d45d8dc4c168
Orel Yosupov <Orely4242@gmail.com>
2023-11-17 10:51:42 +0200

    EventsManager: EventsManagerServer: Create OleDbContext.cs
    First ever object in this Project! the purpose of this file is to connect with Access Database

---
+++ EventsManagerServer/Data_Access_Layer/OleDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+
+namespace EventsManager.Data_Access_Layer
+{
+    public class OleDbContext
+    {
+    }
+}
