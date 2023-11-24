37fd50fb8a1d04460826b6e137bba6a5907753cb
Orel Yosupov <Orely4242@gmail.com>
2023-11-24 10:47:06 +0200

    EventsManager: EventsManagerServer: Create OrderModelCreator.cs
    Add the actual Model Creator

--- EventsManagerServer/Data_Access_Layer/OrderModelCreator.cs
+++ EventsManagerServer/Data_Access_Layer/OrderModelCreator.cs
@@ -12,7 +12,17 @@ namespace EventsManager.Data_Access_Layer
     {
         public Order CreateModel(IDataReader source)
         {
-            throw new NotImplementedException();
+            Order order = new Order()
+            {
+                OrderId = Convert.ToInt16(source["UserId"]),
+                OrderDate = Convert.ToString(source["OrderDate"]),
+                MenuId = Convert.ToInt16(source["MenuId"]),
+                HallId = Convert.ToInt16(source["HallId"]),
+                UserId = Convert.ToInt16(source["UserId"]),
+                RatingId = Convert.ToInt16(source["RatingId"]),
+                NumOfPeople = Convert.ToInt16(source["NumOfPeople"]),
+            };
+            return order;
         }
     }
 }
