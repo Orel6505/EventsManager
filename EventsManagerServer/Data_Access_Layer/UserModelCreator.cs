48bfe4733820a5b12b0b32f3a2bf8d55ebaf7241
Orel Yosupov <Orely4242@gmail.com>
2023-11-24 10:33:52 +0200

    EventsManager: EventsManagerServer: Update UserModelCreator.cs
    Add the actual User Model Creator

--- EventsManagerServer/Data_Access_Layer/UserModelCreator.cs
+++ EventsManagerServer/Data_Access_Layer/UserModelCreator.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using EventsManagerModels;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EventsManager.Data_Access_Layer
 {
-    internal interface UserModelCreator
+    public class UserModelCreator : IOleDbModelCreator<User>
     {
+        public User CreateModel(IDataReader source)
+        {
+            User user = new User()
+            {
+                UserId = Convert.ToInt16(source["UserId"]),
+                FirstName = Convert.ToString(source["UserFirstName"]),
+                LastName = Convert.ToString(source["UserLastName"]),
+                UserName = Convert.ToString(source["UserName"]),
+                PassWordHash = Convert.ToString(source["Password"]),
+                Email = Convert.ToString(source["Email"]),
+                Address = Convert.ToString(source["Address"]),
+                PhoneNum = Convert.ToString(source["PhoneNum"]),
+                CreationDate = Convert.ToString(source["CreationDate"]),
+                Order = null
+            };
+            return user;
+        }
     }
 }
