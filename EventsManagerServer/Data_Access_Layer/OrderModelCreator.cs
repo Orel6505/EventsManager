e39cb83b39ebf7bf27c1473cc6bfcbe9e81de1da
Orel Yosupov <Orely4242@gmail.com>
2023-11-24 10:41:45 +0200

    EventsManager: EventsManagerServer: Create OrderModelCreator.cs
    Order Model Creator

---
+++ EventsManagerServer/Data_Access_Layer/OrderModelCreator.cs
@@ -0,0 +1,18 @@
+﻿using EventsManagerModels;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsManager.Data_Access_Layer
+{
+    public class OrderModelCreator : IOleDbModelCreator<Order>
+    {
+        public Order CreateModel(IDataReader source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
