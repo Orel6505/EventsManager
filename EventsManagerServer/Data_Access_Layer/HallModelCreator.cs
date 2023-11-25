f2b501cf00eedab159aad437ffefa13c27acbadf
Orel Yosupov <Orely4242@gmail.com>
2023-11-25 17:56:48 +0200

    EventsManager: EventsManagerServer: Create HallModelCreator.cs
    Add the actual Model Creator

---
+++ EventsManagerServer/Data_Access_Layer/HallModelCreator.cs
@@ -0,0 +1,17 @@
+﻿using EventsManagerModels;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace EventsManager.Data_Access_Layer
+{
+    public class HallModelCreator : IOleDbModelCreator<Hall>
+    {
+        public Hall CreateModel(IDataReader source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
