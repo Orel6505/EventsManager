a16d8c5ae6902c521ba51cfee57a3154c0c50bc9
Orel Yosupov <Orely4242@gmail.com>
2023-11-25 18:50:27 +0200

    EventsManager: EventsManagerServer: Update MenuModelCreator.cs
    Menu Model Creator

--- EventsManagerServer/Data_Access_Layer/MenuModelCreator.cs
+++ EventsManagerServer/Data_Access_Layer/MenuModelCreator.cs
@@ -11,7 +11,16 @@ namespace EventsManager.Data_Access_Layer
     {
         public Menu CreateModel(IDataReader source)
         {
-            throw new NotImplementedException();
+            Menu menu = new Menu()
+            {
+                MenuId = Convert.ToInt16(source["MenuId"]),
+                MenuName = Convert.ToString(source["MenuName"]),
+                MenuDesc = Convert.ToString(source["MenuDesc"]),
+                MenuPrice = Convert.ToInt16(source["MenuPrice"]),
+                MenuImage = Convert.ToString(source["MenuImage"]),
+                Foods = null
+            };
+            return menu;
         }
     }
 }
