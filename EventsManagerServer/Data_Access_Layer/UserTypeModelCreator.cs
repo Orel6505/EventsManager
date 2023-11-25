6b55ad79cdd0e6fb1d7c6dbdb3dfffa0b8697b26
Orel Yosupov <Orely4242@gmail.com>
2023-11-25 20:49:27 +0200

    EventsManager: EventsManagerServer: Create UserTypeModelCreator.cs
    UserType Model Creator

---
+++ EventsManagerServer/Data_Access_Layer/UserTypeModelCreator.cs
@@ -0,0 +1,17 @@
+﻿using EventsManagerModels;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace EventsManager.Data_Access_Layer
+{
+    public class UserTypeModelCreator : IOleDbModelCreator<UserType>
+    {
+        public UserType CreateModel(IDataReader source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
