50906eee90a329cf66590bd934ece0b664f16f7b
Orel Yosupov <Orely4242@gmail.com>
2023-11-25 20:40:01 +0200

    EventsManager: EventsManagerServer: Create MessageModelCreator.cs
    Add the actual Message Model Creator

---
+++ EventsManagerServer/Data_Access_Layer/MessageModelCreator.cs
@@ -0,0 +1,17 @@
+﻿using EventsManagerModels;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace EventsManager.Data_Access_Layer
+{
+    public class MessageModelCreator : IOleDbModelCreator<Rating>
+    {
+        public Rating CreateModel(IDataReader source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
