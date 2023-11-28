70730c2dd033ccea7de948c66549d5a6de37fbe3
Orel Yosupov <Orely4242@gmail.com>
2023-11-28 20:48:52 +0200

    EventsManager: EvensManagerModels: Create ReviewImages.cs

---
+++ EventsManagerModels/RatingImage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsManagerModels
+{
+    public class RatingImage
+    {
+        public int ImageId { get; set; }
+        public string ImageLocation { get; set; } 
+        public int RatingId { get; set; }
+    }
+}
