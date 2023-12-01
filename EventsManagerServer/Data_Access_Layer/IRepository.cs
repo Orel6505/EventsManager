c35633abc2a046aabb2d2ae138b8bd94b428491b
Orel Yosupov <Orely4242@gmail.com>
2023-12-01 11:35:13 +0200

    EventsManager: EventsManagerServer: Update IRepository.cs
    Add the interface methods that the class that implement will be forced to use - behavior

--- EventsManagerServer/Data_Access_Layer/IRepository.cs
+++ EventsManagerServer/Data_Access_Layer/IRepository.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace EventsManager.Data_Access_Layer
 {
-    internal interface IRepository
+    public interface IRepository<T>
     {
+        //this part is responsible for the logic part in Data access layer
+        //CRUD - Create Read Update Delete
+
+        //Create 
+        bool Insert();
+
+        //Read
+        List<T> GetAll(); //all set
+        T Read(object id); //only a specific set
+        object ReadValue(); //only a specific value
+
+        //Update
+        bool Update();
+
+        //Delete
+        bool Delete();
     }
 }
