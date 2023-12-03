d9091d065e56667e02b0ca730c314cfefe048075
Orel Yosupov <Orely4242@gmail.com>
2023-12-03 14:08:32 +0200

    EventsManager: EventsManagerServer: Create RatingImagesModelCreator.cs
    Create the file of the model factory

---
+++ EventsManagerServer/Data_Access_Layer/ModelFactory/RatingImagesModelCreator.cs
@@ -0,0 +1,18 @@
+﻿
+﻿using EventsManagerModels;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace EventsManager.Data_Access_Layer
+{
+    public class RatingImagesModelCreator : IOleDbModelCreator<RatingImage>
+    {
+        public RatingImage CreateModel(IDataReader source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
