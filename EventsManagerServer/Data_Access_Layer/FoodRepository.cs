cd0c874d25d0d33737db08ad6e0d016bb1fde328
Orel Yosupov <Orely4242@gmail.com>
2023-12-25 10:49:34 +0200

    EventsManager: EventsManagerServer: Add FoodRepository Functions

--- EventsManagerServer/Data_Access_Layer/FoodRepository.cs
+++ EventsManagerServer/Data_Access_Layer/FoodRepository.cs
@@ -1,8 +1,12 @@
 ﻿using EventsManagerModels;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Web;
+using System.Web.DynamicData;
+using System.Web.UI.WebControls;
 
 namespace EventsManager.Data_Access_Layer
 {
@@ -10,27 +14,48 @@ namespace EventsManager.Data_Access_Layer
     {
         public bool Delete(int id)
         {
-            throw new NotImplementedException();
+            string sql = $"DELETE FROM Foods WHERE FoodId=@FoodId";
+            this.AddParameters("FoodId", id.ToString()); //prevents SQL Injection
+            return this.dbContext.Delete(sql) > 0;
         }
 
         public bool Delete(string id)
         {
-            throw new NotImplementedException();
+            return false;
         }
 
-        public List<Food> GetAll()
+        public bool Delete(Food model)
         {
-            throw new NotImplementedException();
+            string sql = $"DELETE FROM Foods WHERE FoodId=@FoodId";
+            this.AddParameters("FoodId", model.FoodId.ToString()); //prevents SQL Injection
+            return this.dbContext.Delete(sql) > 0;
+        }
+
+        public List<Food> ReadAll()
+        {
+            List<Food> Foods = new List<Food>();
+            string sql = "SELECT * FROM Foods";
+            IDataReader dataReader = this.dbContext.Read(sql);
+            while (dataReader.Read() == true)
+                Foods.Add(this.modelFactory.FoodModelCreator.CreateModel(dataReader));
+            return Foods;
         }
 
         public bool Insert(Food model)
         {
-            throw new NotImplementedException();
+            string sql = $"INSERT INTO Foods(FoodName,FoodDesc,FoodImage) VALUES('@FoodName','@FoodDesc','@FoodImage')";
+            this.AddParameters("FoodName", model.FoodName); //prevents SQL Injection
+            this.AddParameters("FoodDesc", model.FoodDesc); //prevents SQL Injection
+            this.AddParameters("FoodImage", model.FoodImage); //prevents SQL Injection
+            return this.dbContext.Create(sql) > 0;
         }
 
         public Food Read(object id)
         {
-            throw new NotImplementedException();
+            string sql = $"SELECT FROM Foods WHERE FoodId=@FoodId";
+            this.AddParameters("FoodId", id.ToString()); //prevents SQL Injection
+            return this.modelFactory.FoodModelCreator.CreateModel(this.dbContext.Read(sql));
+            //returns food
         }
 
         public object ReadValue()
@@ -40,7 +65,12 @@ namespace EventsManager.Data_Access_Layer
 
         public bool Update(Food model)
         {
-            throw new NotImplementedException();
+            string sql = "UPDATE Foods SET FoodName=@FoodName, FoodDesc=@FoodDesc, FoodImage=@FoodId where FoodId=@FoodId";
+            this.AddParameters("FoodId", model.FoodId.ToString()); //prevents SQL Injection
+            this.AddParameters("FoodName", model.FoodName); //prevents SQL Injection
+            this.AddParameters("FoodDesc", model.FoodDesc); //prevents SQL Injection
+            this.AddParameters("FoodImage", model.FoodImage); //prevents SQL Injection
+            return this.dbContext.Update(sql) > 0;
         }
     }
 }
