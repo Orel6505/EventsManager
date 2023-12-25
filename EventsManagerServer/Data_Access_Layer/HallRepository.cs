f6f1d3f8b535b98f46cf1db97d84ad5a27d736c7
Orel Yosupov <Orely4242@gmail.com>
2023-12-25 10:57:02 +0200

    EventsManager: EventsManagerServer: Create HallRepository.cs

---
+++ EventsManagerServer/Data_Access_Layer/HallRepository.cs
@@ -0,0 +1,51 @@
+﻿using EventsManagerModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventsManager.Data_Access_Layer
+{
+    public class HallRepository : Repository, IRepository<Hall>
+    {
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Hall model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(Hall model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Hall Read(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Hall> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ReadValue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Hall model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
