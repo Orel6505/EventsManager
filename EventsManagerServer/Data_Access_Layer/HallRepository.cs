139c4d59e4aa031a4b7fbc890e52910c0e1e2747
Orel Yosupov <Orely4242@gmail.com>
2023-12-25 10:58:50 +0200

    EventsManager: EventsManagerServer: Add HallRepository Functions

--- EventsManagerServer/Data_Access_Layer/HallRepository.cs
+++ EventsManagerServer/Data_Access_Layer/HallRepository.cs
@@ -1,6 +1,7 @@
 ﻿using EventsManagerModels;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 
@@ -10,32 +11,49 @@ namespace EventsManager.Data_Access_Layer
     {
         public bool Delete(int id)
         {
-            throw new NotImplementedException();
+            string sql = $"DELETE FROM Halls WHERE HallId=@HallId";
+            this.AddParameters("HallId", id.ToString()); //prevents SQL Injection
+            return this.dbContext.Delete(sql) > 0;
         }
 
         public bool Delete(string id)
         {
-            throw new NotImplementedException();
+            return false;
         }
 
         public bool Delete(Hall model)
         {
-            throw new NotImplementedException();
+            string sql = $"DELETE FROM Halls WHERE HallId=@HallId";
+            this.AddParameters("HallId", model.HallId.ToString()); //prevents SQL Injection
+            return this.dbContext.Delete(sql) > 0;
         }
 
         public bool Insert(Hall model)
         {
-            throw new NotImplementedException();
+            string sql = $"INSERT INTO Halls(HallName,HallDesc,HallImage,MaxPeople) VALUES('@HallName','@HallDesc','@HallImage',@MaxPeople)";
+            this.AddParameters("HallName", model.HallName); //prevents SQL Injection
+            this.AddParameters("HallDesc", model.HallDesc); //prevents SQL Injection
+            this.AddParameters("HallImage", model.HallImage); //prevents SQL Injection
+            this.AddParameters("HallImage", model.MaxPeople.ToString()); //prevents SQL Injection
+            return this.dbContext.Create(sql) > 0;
         }
 
         public Hall Read(object id)
         {
-            throw new NotImplementedException();
+            string sql = $"SELECT FROM Halls WHERE HallId=@HallId";
+            this.AddParameters("HallId", id.ToString()); //prevents SQL Injection
+            return this.modelFactory.HallModelCreator.CreateModel(this.dbContext.Read(sql));
+            //returns food
         }
 
         public List<Hall> ReadAll()
         {
-            throw new NotImplementedException();
+            List<Hall> Halls = new List<Hall>();
+            string sql = "SELECT * FROM Halls";
+            IDataReader dataReader = this.dbContext.Read(sql);
+            while (dataReader.Read() == true)
+                Halls.Add(this.modelFactory.HallModelCreator.CreateModel(dataReader));
+            return Halls;
         }
 
         public object ReadValue()
@@ -45,7 +63,13 @@ namespace EventsManager.Data_Access_Layer
 
         public bool Update(Hall model)
         {
-            throw new NotImplementedException();
+            string sql = "UPDATE Halls SET HallName=@HallName, HallDesc=@HallDesc, HallImage=@HallImage, MaxPeople=@MaxPeople where HallId=@HallId";
+            this.AddParameters("HallId", model.HallId.ToString()); //prevents SQL Injection
+            this.AddParameters("HallName", model.HallName); //prevents SQL Injection
+            this.AddParameters("HallDesc", model.HallDesc); //prevents SQL Injection
+            this.AddParameters("HallImage", model.HallImage); //prevents SQL Injection
+            this.AddParameters("HallImage", model.MaxPeople.ToString()); //prevents SQL Injection
+            return this.dbContext.Update(sql) > 0;
         }
     }
 }
