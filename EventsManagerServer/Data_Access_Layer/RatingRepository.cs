8d7a10fb1b563603b04ac3d13690d6f3b56168c1
Orel Yosupov <Orely4242@gmail.com>
2023-12-28 19:06:08 +0200

    EventsManager: EventsManagerServer: Create RatingRepository.cs

---
+++ EventsManagerServer/Data_Access_Layer/RatingRepository.cs
@@ -0,0 +1,12 @@
+﻿using EventsManagerModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventsManager.Data_Access_Layer
+{
+    public class RatingRepository : Repository, IRepository<Rating>
+    {
+    }
+}
