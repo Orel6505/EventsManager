1e348a0f17b4ff6a4aa4815ca3e4a84cfc7f8731
Orel Yosupov <Orely4242@gmail.com>
2024-05-03 09:04:17 +0300

    EventsManager: Server: Remove Messages
    No Time To Explain!

--- CommonParameters/CommonParameters.cs
+++ CommonParameters/CommonParameters.cs
@@ -8,7 +8,7 @@ namespace CommonParameters
 {
     public class Location
     {
-        public static readonly string Db = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\Orely\\Desktop\\EM-Main\\EventsManagerServer\\App_Data\\Database.accdb";
+        public static readonly string Db = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\Orely\\DesktopStorage\\EM-Main\\EventsManagerServer\\App_Data\\Database.accdb";
         public static readonly string WebService = "http://localhost:58238";
         public static readonly string WebServer = "https://localhost:44365";
 
